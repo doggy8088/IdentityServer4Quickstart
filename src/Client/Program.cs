6f60b5e3c43fb4b65cf52d82537e2bf379abd518
Will <doggy.huang@gmail.com>
2022-04-05 02:26:57 +0800

    加入 Client 專案

---
+++ src/Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+}
