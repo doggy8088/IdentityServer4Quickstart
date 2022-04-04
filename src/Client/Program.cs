56950f4483fe8eaa79923b3059880a30669abe31
Will <doggy.huang@gmail.com>
2022-04-05 02:27:29 +0800

    取得 IdentityServer4 上面的所有端點資訊

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -9,6 +9,14 @@ namespace Client
     {
         static async Task Main(string[] args)
         {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
         }
     }
 }
